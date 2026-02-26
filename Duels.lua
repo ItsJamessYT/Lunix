d6bc2a6ba2f05b4d3c3b8c9956a93d674d272d65
James <murganaleandro@gmail.com>
2026-02-26 17:51:11 -0300

    Update Duels.lua

--- Duels.lua
+++ Duels.lua
@@ -1,2 +1,2 @@
 -- key system?
-loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/ceabad18e4ac441c"))()
+loadstring(game:HttpGet("https://monlua-protector.vercel.app/raw/76695dba840cff31e3c12e94eec4d233"))()
