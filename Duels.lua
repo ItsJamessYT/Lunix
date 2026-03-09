cae164218378eceeb8e8a37b2cad5159a17162e9
James <murganaleandro@gmail.com>
2026-03-09 14:22:05 -0300

    Update Duels.lua

--- Duels.lua
+++ Duels.lua
@@ -1,2 +1,1 @@
-getgenv().SCRIPT_KEY = "LunixDuelsFree"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/d0a961a6bd2b3971e3e08ab902abce47188cc9d74bac02f4b811255006f6d613/download"))()
