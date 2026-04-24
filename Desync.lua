2dc7972e9ea58bd495bc7cead4ead24f61e574ab
James <murganaleandro@gmail.com>
2026-04-24 10:41:56 -0300

    Update Desync.lua

--- Desync.lua
+++ Desync.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/b01fc0497bba7f19474b33f66a701207437421e123f4ca5c1b7f3a89da7245c5/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/dbdf132d02839e917c6e1ba6dec98bcc4dcb88ac0baad5dfd191b7fc5d8db41e/download"))()
