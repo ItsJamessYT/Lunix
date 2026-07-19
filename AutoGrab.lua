dfe436fd643ca55965fa4a21f49eb5afeffd81c8
James <murganaleandro@gmail.com>
2026-07-19 00:00:46 -0300

    Update and rename AutoGrab.lua to LunixTools.lua

--- AutoGrab.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://pastefy.app/VBMWHaLA/raw",true))()
--- Not a logger, just lazy to publish in other place!
---
+++ LunixTools.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://pastefy.app/w6qc9of9/raw",true))()
+-- Not a logger, just lazy to publish in other place!
